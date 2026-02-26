4b6ace61dacbc63406b85440871edbe1136dfc1b
stijn1290 <153715940+stijn1290@users.noreply.github.com>
2026-02-26 21:11:44 +0100

    W-2 added logo and removed more standard content

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <script setup>
+import Logo from "./components/Logo.vue";
 </script>
 
 <template>
+  <header class="flex justify-center items-center w-full">
+    <Logo/>
+  </header>
   <h2 class="text-white p-46">Setup :)</h2>
 </template>
