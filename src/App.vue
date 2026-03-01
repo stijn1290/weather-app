1b6feecd8d78ea9107b49d1e5117e9686815a5cf
stijn1290 <153715940+stijn1290@users.noreply.github.com>
2026-03-01 19:10:37 +0100

    W-10 Added additional metrics and the stats with emitting

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,10 @@ import AditionalMetrics from "./components/AditionalMetrics.vue";
 const selectedPlace = ref("");
 const temperature = ref("");
 const country = ref("");
+const feelsLike = ref("");
+const humidity = ref("");
+const windSpeed = ref("");
+const precipitation = ref("");
 </script>
 
 <template>
@@ -16,12 +20,14 @@ const country = ref("");
   <main class="flex flex-col gap-10">
     <h1 class="text-5xl text-white font-medium text-center">How's the sky looking today?</h1>
     <section class="flex flex-col justify-center items-center">
-      <SearchBar @location="selectedPlace = $event" @temperature="temperature = $event" @country="country = $event" />
+      <SearchBar @location="selectedPlace = $event" @temperature="temperature = $event"
+                 @country="country = $event" @feelsLike="feelsLike = $event" @humidity="humidity = $event"
+       @windSpeed="windSpeed = $event" @precipitation="precipitation = $event"/>
     </section>
     <section class="grid grid-cols-[0.8fr_0.5fr]">
       <div class="flex flex-col gap-10">
         <WeatherConditions :location="selectedPlace" :temperature="temperature" :country="country" />
-        <AditionalMetrics/>
+        <AditionalMetrics :feelsLike="feelsLike" :humidity="humidity" :windSpeed="windSpeed" :precipitation="precipitation" />
       </div>
     </section>
   </main>
--- src/components/AditionalMetrics.vue
+++ src/components/AditionalMetrics.vue
@@ -1,14 +1,20 @@
 <script setup lang="ts">
 
 import Metric from "./Metric.vue";
+defineProps({
+  feelsLike: String,
+  humidity: String,
+  windSpeed: String,
+  precipitation: String,
+})
 </script>
 
 <template>
   <div class="grid grid-cols-4 gap-4">
-    <Metric :title="'Feels Like'" :stat="'°'"/>
-    <Metric :title="'Humidity'" :stat="'%'"/>
-    <Metric :title="'Wind'" :stat="'km/h'"/>
-    <Metric :title="'Precipitation'" :stat="'mm'"/>
+    <Metric :title="'Feels Like'" :stat="feelsLike + '°'"/>
+    <Metric :title="'Humidity'" :stat="humidity + '%'"/>
+    <Metric :title="'Wind'" :stat="windSpeed + 'km/h'"/>
+    <Metric :title="'Precipitation'" :stat="precipitation + 'mm'"/>
   </div>
 </template>
 
